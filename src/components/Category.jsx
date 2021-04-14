55d29bd83dac4c9a4b379b0c934e6325096d6782
Sami <salidelgado@paradigmadigital.com>
2021-04-14 22:18:48 +0200

    finishing the project

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './../assets/styles/components/Category.scss';
 
-const Category = ({title}) =>(
+const Category = ({title, children}) =>(
   <div className="categories">
       <h3 className="categories__title">{title}</h3>
+      {children}
   </div>
 
 
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,44 +1,53 @@
-import React from 'react'
+import React ,{useEffect, useState} from 'react'
 import Header from './../components/Header';
 import Search from './../components/Search';
 import Category from './../components/Category';
 import Carousel from './../components/Carousel';
 import CarouselItem from './../components/CarouselItem';
 import Footer from './../components/Footer';
+
 import './../assets/styles/App.scss';
 
-const App = () => (
-	<div className="App">
-		<Header/>
-		<Search/>
-		<Category title="Originales de Platzi"/>
-		<Carousel>
-			<CarouselItem/>
-			<CarouselItem/>
-			<CarouselItem/>
-			<CarouselItem/>
-			<CarouselItem/>
-			<CarouselItem/>
-			<CarouselItem/>
-		</Carousel>
+const App = () => {
 
-			<Category title="Originales de Netflix"/>
-			<Carousel>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-			</Carousel>
+	const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
 
-			<Category title="Nueva Colección"/>
-			<Carousel>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-			</Carousel>
-		<Footer/>
-	</div>
-);
+	useEffect(() => {
+		fetch('http://localhost:3000/initalState')
+			.then(response => response.json())
+			.then(data => setVideos(data))
+	
+	}, []);
+
+	return(
+		<div className="App">
+			<Header/>
+			<Search/>
+			{ videos.mylist.length > 0 &&
+				<Category title="Originales de Platzi">
+					<Carousel>
+						<CarouselItem/>
+					</Carousel>
+				</Category>
+			}
+			<Category title="Tendencias de Platzi">
+				<Carousel>
+					{videos.trends.map(item =>
+						<CarouselItem key={item.id} {...item} />
+					)}
+					
+				</Carousel>
+			</Category>
+			<Category title="Originales de Netflix">
+				<Carousel>
+				{videos.originals.map(item =>
+						<CarouselItem key={item.id} {...item} />
+					)}
+				</Carousel>
+			</Category>
+			<Footer/>
+		</div>
+	);
+}
 
 export default App;
